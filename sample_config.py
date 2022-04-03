638d93806b8595047b6803f534f3d0e5c8644649
razudelwar <47747273+razudelwar@users.noreply.github.com>
2022-04-03 14:38:33 +0600

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,10 +5,10 @@ import os
 
 class Config((object)):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = "TG_BOT_TOKEN"
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = "APP_ID"
+    API_HASH = "API_HASH"
     # Get these values from my.telegram.org
     # Banned Unwanted Members..
     BANNED_USERS = {int(x) for x in os.environ.get("BANNED_USERS", "").split()}
@@ -34,11 +34,11 @@ class Config((object)):
     # watermark file
     DEF_WATER_MARK_FILE = ""
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = "OWNER_ID"
     # database session name, example: xurluploader
-    SESSION_NAME = os.environ.get("SESSION_NAME", "")
+    SESSION_NAME = "SESSION_NAME"
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = "DATABASE_URL"
     PROCESS_TEXT = """
     Process: {}
     """
